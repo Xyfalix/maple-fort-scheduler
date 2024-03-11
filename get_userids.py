0d394ad842a19d98efb2c56b9b0367e629a68540
Xyfalix <129175727+Xyfalix@users.noreply.github.com>
2024-03-11 12:05:59 +0800

    chore: clean up unused files and refactor code

--- get_userids.py
+++ get_userids.py
@@ -1,17 +1,19 @@
 from telethon import TelegramClient
 import asyncio
+import os
 
 api_id = "24143587" 
 api_hash = "f5cbe44c18b32dc5d54ae3e2c407d9af" # Your APP_ID
-bot_token = "6986262473:AAGkbAyzypzNJ_6vsPzuUSDvY7glThxL3FA" # (via botfather, you can alternatively sign in as a user)
 group_id = -1001646346699
+TOKEN = os.environ.get('TOKEN')
+
 
 async def get_users(client, group_id):
     async for user in client.iter_participants(group_id):
         if not user.deleted:
             print("id:", user.id, "username:", user.username) 
 
-bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
+bot = TelegramClient('bot', api_id, api_hash).start(bot_token=TOKEN)
 
 with bot:
     asyncio.get_event_loop().run_until_complete(get_users(bot, group_id))
--- scheduler-EC2.py
+++ scheduler-EC2.py
@@ -1,46 +1,29 @@
-from flask import Flask, request
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes, CallbackQueryHandler, ConversationHandler
-from telegram.error import TelegramError
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
-from datetime import datetime, timedelta, time
+from datetime import datetime, time
 import pytz
 
 load_dotenv()
 TOKEN = os.environ.get('TOKEN')
-BOT_USERNAME: os.environ.get('BOT_USERNAME')
+BOT_USERNAME = os.environ.get('BOT_USERNAME')
 MONGO_URI = os.environ.get('DATABASE_URL')
 DATABASE_NAME = os.environ.get('DATABASE_NAME')
-FORT_SCHEDULE_ARRAY = ["BF1", "BF3", "BF5", "BF5"]
+FORT_SCHEDULE_ARRAY = ["BF1", "BF1", "BF5", "BF5", "BF5", "BF5", "BF5", "BF5"]
 # set states for ConversationHandler for user replacement
 SELECT_SECTION, SELECT_USER_TO_REPLACE, TYPE_IN_REPLACEMENT = range(3)
 # set state for ConversationHandler for fort acknowledgement
 MARK_ATTENDANCE = 0
 EXILES_ADMIN_CHAT_ID = -1001646346699
 EXILES_MAIN_CHAT_ID = -1001866482989
-index = 0 # index to determine which bf list the temp list will be drawing data from
+NICHOLAS_CHAT_ID = 622759708
 
 client = MongoClient(MONGO_URI)
 db = client[DATABASE_NAME]
 
-app = Flask(__name__)
-
 # Commands
-# user initiated command
-async def add_user_command(update:Update, context: ContextTypes.DEFAULT_TYPE):
-    command_text = update.message.text
-    print(command_text)
-    username = command_text.replace("/adduser", "").strip()
-
-    new_user = User(username=username)
-    new_user.save_to_db()
-
-    response = f"User {username} added successfully!"
-    print(f"Bot: {response}")
-    await update.message.reply_text(response)
-
 # user initiated command
 async def view_fort_list(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await generate_temp_fort_list(context)
@@ -108,13 +91,20 @@ async def check_attendance(update: Update, context: ContextTypes.DEFAULT_TYPE):
 # scheduled command
 # duplicate either BF1, BF3, or BF5 depending on week and set as temp list for viewing and editing
 async def duplicate_fort_list(context: ContextTypes.DEFAULT_TYPE):
-    global index
+    index_collection = db.index
+    print("Pulling fort list...")
+
+    # retrieve index doc
+    index_doc = index_collection.find_one({"name": "index"})
+    index_value = index_doc.get("value", "error retrieving value")
+    print(f'index_value is {index_value}')
+
     try:
         fortlist = db.fortlists
         print("Pulling fort list...")
 
         # Find the fort for the week
-        fort_list = fortlist.find_one({"name": FORT_SCHEDULE_ARRAY[index]})
+        fort_list = fortlist.find_one({"name": FORT_SCHEDULE_ARRAY[index_value]})
 
         temp_fort_list = fort_list.copy()
 
@@ -123,8 +113,6 @@ async def duplicate_fort_list(context: ContextTypes.DEFAULT_TYPE):
 
         result = fortlist.insert_one(temp_fort_list)
 
-        index = (index + 1) % len(FORT_SCHEDULE_ARRAY)
-
         if result.inserted_id:
             print(f"Successfully duplicated BFList. New name: temp")
         else:
@@ -132,21 +120,41 @@ async def duplicate_fort_list(context: ContextTypes.DEFAULT_TYPE):
 
     except Exception as e:
         print(f"Error querying MongoDB: {e}")
-    
 
+async def increment_index(context: ContextTypes.DEFAULT_TYPE):
+    try:
+        index_collection = db.index
+        index_doc = index_collection.find_one({"name": "index"})
+        index_value = index_doc.get("value", "error retrieving value")
+        index_value= (index_value + 1) % len(FORT_SCHEDULE_ARRAY)
+
+        # Update the index value in the MongoDB document
+        index_collection.update_one(
+            {"name": "index"},
+            {"$set": {"value": index_value}}
+        )
+    except Exception as e:
+        print(f"Error querying MongoDB: {e}")
+    
 # scheduled command
 # generate fort list for display on telegram                
 async def generate_temp_fort_list(context: ContextTypes.DEFAULT_TYPE):
     try:
         fortlist = db.fortlists
+        index_collection = db.index
         print("Pulling fort list...")
+
+        # retrieve index doc
+        index_doc = index_collection.find_one({"name": "index"})
+        index_value = index_doc.get("value", "error retrieving value")
+        print(f'index_value is {index_value}')
 
         # Find the document with the specified name
         bf_list_document = fortlist.find_one({"name": "temp"})
 
         if bf_list_document:
             # Access the data from the BFList document
-            name = FORT_SCHEDULE_ARRAY[index]
+            name = FORT_SCHEDULE_ARRAY[index_value]
 
             # Process the data as needed
             print(f"Fort List Name: {name}")
@@ -206,12 +214,11 @@ async def generate_temp_fort_list(context: ContextTypes.DEFAULT_TYPE):
     except Exception as e:
         print(f"Error querying MongoDB: {e}")
 
-
 # scheduled command
 async def send_fort_list(context: ContextTypes.DEFAULT_TYPE):
     fort_list = context.bot_data.get('fort_list', 'fort list not found')
     # change chat_id to the group that you want to send it to
-    await context.bot.send_message(chat_id=622759708, text=fort_list)
+    await context.bot.send_message(chat_id=NICHOLAS_CHAT_ID, text=fort_list)
 
 
 # scheduled command
@@ -256,6 +263,7 @@ async def start_acknowledge(update: Update, context: ContextTypes.DEFAULT_TYPE):
     
     if user_not_found:
         print(f"user {username} is not registered for fort today!")
+        await update.message.reply_text(f"user {username} is not registered for fort today!")
     else:
         keyboard = [
             [
@@ -435,57 +443,6 @@ async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await query.edit_message_text(text="See you next time!")
     return ConversationHandler.END
 
-# scheduled command
-# Define the /send_reminders command
-async def send_ack_reminders(context: ContextTypes.DEFAULT_TYPE):
-    print(f"send_ack_reminders is running")
-    # identify the users who have not acknowledged
-    try:
-        # create blank lists to store telehandles of users who have not acknowledged
-        not_acknowledged = []
-
-        fortlist = db.fortlists
-        userlist = db.users
-
-        print("Pulling fort list...")
-        # Find the document with the specified name
-        temp_fortlist = fortlist.find_one({"name": "temp"})
-
-        if temp_fortlist:
-            # Access the data from the BFList document
-            for section_name, users in temp_fortlist.items():
-                if section_name not in ['_id', 'name']:
-                    # Check for users who have not acknowledged or cannot attend
-                    for user in users:
-                        if user.get('attendance') == "":
-                            not_acknowledged.append(user.get('telehandle'))
-            # generate list of mbrs who have not acked
-            print(f"list of members who have not acknowledged: {not_acknowledged}")
-            for telehandle in not_acknowledged:
-                user_data = userlist.find_one({'username': telehandle})
-                if user_data:
-                    chat_id = user_data.get('chat_id')
-                    print(f"{telehandle}'s chat_id is {chat_id}")
-                    # await context.bot.send_message(chat_id=chat_id, text="Hi there, you have not acknowledged your attendance for fort, please do so ASAP :)")
-
-        else:
-            print("temp fort list not found in the database")
-
-
-    except Exception as e:
-        print(f"Error querying MongoDB: {e}")
-
-    # chat_ids = [622759708, 243938894, 1317136408]  # Replace with your list of chat IDs
-
-    # for chat_id in chat_ids:
-    #     try:
-    #         await context.bot.send_message(chat_id, text="Hi there, you have not acknowledged your attendance for fort, please do so ASAP :)")
-    #     except TelegramError as e:
-    #         if e.message.startswith("Forbidden"):
-    #             print(f"Chat ID {chat_id} has restrictions on bot-initiated conversations.")
-    #         else:
-    #             print(f"Error occurred while sending message to {chat_id}: {e}")
-
 
 # scheduled command
 async def send_replace_reminders(context: ContextTypes.DEFAULT_TYPE):
@@ -530,129 +487,110 @@ async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print(f"Update {update} caused error {context.error}")
 
 
-@app.route('/', methods=['GET', 'POST'])
 def main():
-    if request.method == 'POST':
-        print('Starting bot...')
-        app = Application.builder().token(TOKEN).build()
-        job_queue = app.job_queue
-        timezone_utc8 = pytz.timezone('Asia/Singapore')
+    print('Starting bot...')
+    app = Application.builder().token(TOKEN).build()
+    job_queue = app.job_queue
+    timezone_utc8 = pytz.timezone('Asia/Singapore')
 
-        # scheduled jobs
+    # scheduled jobs
 
-        # check if temp list exists and delete it if it exists
-        # prod code 
-        # remove_temp_fort_list = job_queue.run_daily(delete_temp_fort_list, time=time(hour=23, minute=50), days=(5, 6))
+    # check if temp list exists and delete it if it exists
+    # prod code 
+    remove_temp_fort_list = job_queue.run_daily(delete_temp_fort_list, time=time(hour=1, minute=22, tzinfo=timezone_utc8), days=(0, 6))
 
-        # test code to check for functionality
-        remove_temp_fort_list = job_queue.run_once(delete_temp_fort_list, 5)
+    # test code to check for functionality
+    # remove_temp_fort_list = job_queue.run_once(delete_temp_fort_list, 5)
 
-        # # select correct fort list and make a copy and set as temp list for viewing and editing
-        # prod code
-        # make_temp_fort_list = job_queue.run_daily(delete_temp_fort_list, time=time(hour=23, minute=55), days=(5, 6))
+    # # select correct fort list and make a copy and set as temp list for viewing and editing
+    # prod code
+    make_temp_fort_list = job_queue.run_daily(duplicate_fort_list, time=time(hour=1, minute=23, tzinfo=timezone_utc8), days=(6, 0))
 
-        # test code
-        make_temp_fort_list = job_queue.run_once(duplicate_fort_list, 10)
-
-
-        # display fort list at 12mn on sat and sun
-        # prod code
-        # generate_fort_list = job_queue.run_daily(generate_temp_fort_list, time=time(hour=0, minute=0, second=0), days=(0, 6))
-        # display_fort_list = job_queue.run_once(send_fort_list, time=time(hour=0, minute=0, second=40), days=(0, 6))
-
-        # test code
-        generate_fort_list = job_queue.run_once(generate_temp_fort_list, 15)
-        display_fort_list = job_queue.run_once(send_fort_list, 25)
-
-        # increment index at Mon 12am
-
-        # send reminders to users who have not acknowledged fort from 12pm to 9pm on an hourly basis on sat and sun
-        # prod code 
-        # run_personal_reminder = job_queue.run_repeating(
-        #     callback=send_ack_reminders,
-        #     interval=3600,
-        #     first=time(hour=12),
-        #     last=time(hour=21),
-        #     days=(6, 0)
-        # )
-        
-        # test code
-        run_personal_reminder = job_queue.run_repeating(
-            callback=send_ack_reminders,
-            interval=60,
-            first=timezone_utc8.localize(datetime.utcnow()) + timedelta(seconds=10)  # Run after 10 seconds
-        )
-
-        # prod code
-        # run_admin_reminder = job_queue.run_repeating(
-        #     callback=send_replace_reminders,
-        #     interval=3600,
-        #     first=time(hour=12),
-        #     last=time(hour=21),
-        #     days=(6, 0) 
-        # )
-
-        # test code
-        # run_admin_reminder = job_queue.run_repeating(
-        #     callback=send_replace_reminders,
-        #     interval=30,
-        #     first=timezone_utc8.localize(datetime.utcnow()) + timedelta(seconds=10)  # Run after 10 seconds
-        # )
-
-        # Commands
-        # Add the /adduser command handler
-        app.add_handler(CommandHandler('adduser', add_user_command))
-        # allow users to view current fort list
-        app.add_handler(CommandHandler('view', view_fort_list))
-        # allow admins to view who has not acknowledged attendance or cannot attend
-        app.add_handler(CommandHandler('chkatd', check_attendance))
+    # test code
+    # make_temp_fort_list = job_queue.run_once(duplicate_fort_list, 10)
 
 
-        ack_handler = ConversationHandler(
-        entry_points=[CommandHandler("ack", start_acknowledge)],
-        states={
-            MARK_ATTENDANCE: [
-                CallbackQueryHandler(cancel, pattern="^" + "Cancel" + "$" ),
-                CallbackQueryHandler(mark_attendance),
-            ],
-        },
-        fallbacks=[CommandHandler("ack", start_acknowledge)],
-        )
+    # display fort list at 12mn on sat and sun
+    # prod code
+    generate_fort_list = job_queue.run_daily(generate_temp_fort_list, time=time(hour=1, minute=24, second=0, tzinfo=timezone_utc8), days=(6, 0))
+    display_fort_list = job_queue.run_daily(send_fort_list, time=time(hour=1, minute=25, second=40, tzinfo=timezone_utc8), days=(6, 0))
 
-        edit_handler = ConversationHandler(
-        entry_points=[CommandHandler("edit", start_edit)],
-        states={
-            SELECT_SECTION: [
-                CallbackQueryHandler(cancel, pattern="^" + "Cancel" + "$" ),
-                CallbackQueryHandler(select_mbr_to_replace),
-                
-            ],
-            SELECT_USER_TO_REPLACE: [
-                CallbackQueryHandler(cancel, pattern="^" + "Cancel" + "$" ),
-                CallbackQueryHandler(select_replacement),
-            ],
-            TYPE_IN_REPLACEMENT: [
-                MessageHandler(filters.TEXT & ~filters.COMMAND, execute_replacement),
-            ]
-        },
-        fallbacks=[CommandHandler("edit", start_edit)],
-        )
+    # test code
+    # generate_fort_list = job_queue.run_once(generate_temp_fort_list, 15)
+    # display_fort_list = job_queue.run_once(send_fort_list, 25)
 
-        # add fort acknowledgement functionality utilizing Conversation Handler
-        app.add_handler(ack_handler)
+    # increment index after fort list is sent
+    # prod code
+    update_fort_list_index = job_queue.run_daily(increment_index, time=time(hour=1, minute=26, second=0, tzinfo=timezone_utc8), days=(6, 0))
 
-        # add edit user functionality utilizing Conversation Handler
-        app.add_handler(edit_handler)
-    
-        # Errors
-        app.add_error_handler(error)
+    # test code
+    # update_fort_list_index = job_queue.run_once(increment_index, 20)
 
-        # Run the bot
-        app.run_polling(poll_interval=5)
+    # prod code
+    # run_admin_reminder = job_queue.run_repeating(
+    #     callback=send_replace_reminders,
+    #     interval=3600,
+    #     first=time(hour=12),
+    #     last=time(hour=21),
+    #     days=(6, 0) 
+    # )
 
-    else:
-        return "<h1>This is a test message</h1>"
+    # test code
+    # run_admin_reminder = job_queue.run_repeating(
+    #     callback=send_replace_reminders,
+    #     interval=30,
+    #     first=timezone_utc8.localize(datetime.utcnow()) + timedelta(seconds=10)  # Run after 10 seconds
+    # )
+
+    # Commands
+    # allow users to view current fort list
+    app.add_handler(CommandHandler('view', view_fort_list))
+    # allow admins to view who has not acknowledged attendance or cannot attend
+    app.add_handler(CommandHandler('chkatd', check_attendance))
+
+
+    ack_handler = ConversationHandler(
+    entry_points=[CommandHandler("ack", start_acknowledge)],
+    states={
+        MARK_ATTENDANCE: [
+            CallbackQueryHandler(cancel, pattern="^" + "Cancel" + "$" ),
+            CallbackQueryHandler(mark_attendance),
+        ],
+    },
+    fallbacks=[CommandHandler("ack", start_acknowledge)],
+    )
+
+    edit_handler = ConversationHandler(
+    entry_points=[CommandHandler("edit", start_edit)],
+    states={
+        SELECT_SECTION: [
+            CallbackQueryHandler(cancel, pattern="^" + "Cancel" + "$" ),
+            CallbackQueryHandler(select_mbr_to_replace),
+            
+        ],
+        SELECT_USER_TO_REPLACE: [
+            CallbackQueryHandler(cancel, pattern="^" + "Cancel" + "$" ),
+            CallbackQueryHandler(select_replacement),
+        ],
+        TYPE_IN_REPLACEMENT: [
+            MessageHandler(filters.TEXT & ~filters.COMMAND, execute_replacement),
+        ]
+    },
+    fallbacks=[CommandHandler("edit", start_edit)],
+    )
+
+    # add fort acknowledgement functionality utilizing Conversation Handler
+    app.add_handler(ack_handler)
+
+    # add edit user functionality utilizing Conversation Handler
+    app.add_handler(edit_handler)
+
+    # Errors
+    app.add_error_handler(error)
+
+    # Run the bot
+    app.run_polling(poll_interval=5)
+
 
 if __name__ == '__main__':
-    # Start the Flask development server
-    app.run(host="0.0.0.0", port=5000)
+    main()
